240b36a654ec726612121cc92a30c1ab044a2842
zebsme <zeb20030@gmail.com>
2024-11-13 18:24:08 +0800

    feat(index): support keydirentry

---
+++ src/index/keydir.rs
@@ -0,0 +1,29 @@
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
+pub struct KeyDirEntry {
+    file_id: u32,
+    offset: u64,
+    size: u32,
+}
+
+impl KeyDirEntry {
+    pub fn new(file_id: u32, offset: u64, size: u32) -> Self {
+        Self {
+            file_id,
+            offset,
+            size,
+        }
+    }
+
+    pub fn get_file_id(&self) -> u32 {
+        self.file_id
+    }
+
+    pub fn get_offset(&self) -> u64 {
+        self.offset
+    }
+
+    pub fn get_size(&self) -> u32 {
+        self.size
+    }
+}
